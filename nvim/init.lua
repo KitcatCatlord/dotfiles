344e82997042f91264a9c86879cb9e81199d205c
Kian <143936856+KitcatCatlord@users.noreply.github.com>
2025-11-21 17:37:12 +0000

    Add plugins to config
    Check diff for more info

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 6
+vim.opt.updatetime = 200 -- NTS: Extra addition
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -17,8 +18,14 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "folke/tokyonight.nvim",                   lazy = false,                                                   priority = 1000,                                                                         config = function()
-    vim.cmd("colorscheme tokyonight") end },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight")
+    end
+  },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -30,8 +37,13 @@ require("lazy").setup({
   { "nvim-treesitter/nvim-treesitter-context", config = function() require("treesitter-context").setup({}) end },
   { "windwp/nvim-ts-autotag",                  config = function() require("nvim-ts-autotag").setup() end },
 
-  { "nvim-lualine/lualine.nvim",               dependencies = { "nvim-tree/nvim-web-devicons" },               config = function()
-    require("lualine").setup({ options = { theme = "tokyonight" } }) end },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({ options = { theme = "tokyonight" } })
+    end
+  },
 
   {
     "akinsho/bufferline.nvim",
@@ -75,23 +87,35 @@ require("lazy").setup({
       require("todo-comments").setup({ keywords = { TODO = { icon = "" }, FIX = { icon = "" }, NOTE = { icon = "" }, NTS = { icon = "", color = "hint", alt = { "NOTICE", "NTS" } } } })
     end
   },
-  { "nvim-mini/mini.icons",        config = function() require("mini.icons").setup() end },
+  { "nvim-mini/mini.icons",    config = function() require("mini.icons").setup() end },
 
-  { "stevearc/oil.nvim",           opts = { view_options = { show_hidden = true } } },
+  { "stevearc/oil.nvim",       opts = { view_options = { show_hidden = true } } },
 
-  { "lewis6991/gitsigns.nvim",     config = function() require("gitsigns").setup() end },
+  { "lewis6991/gitsigns.nvim", config = function() require("gitsigns").setup() end },
 
-  { "numToStr/Comment.nvim",       config = function() require("Comment").setup() end },
+  { "numToStr/Comment.nvim",   config = function() require("Comment").setup() end },
 
-  { "windwp/nvim-autopairs",       config = function() require("nvim-autopairs").setup() end },
+  { "windwp/nvim-autopairs",   config = function() require("nvim-autopairs").setup() end },
 
-  { "kylechui/nvim-surround",      version = "*",                                            config = function() require(
-    "nvim-surround").setup() end },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = function()
+      require(
+        "nvim-surround").setup()
+    end
+  },
 
   { "folke/which-key.nvim",        config = function() require("which-key").setup() end },
 
-  { "folke/trouble.nvim",          dependencies = { "nvim-tree/nvim-web-devicons" },         config = function() require(
-    "trouble").setup() end },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require(
+        "trouble").setup()
+    end
+  },
 
   { "aznhe21/actions-preview.nvim" },
 
@@ -123,9 +147,15 @@ require("lazy").setup({
     end
   },
 
-  { "stevearc/overseer.nvim",  config = function() require("overseer").setup() end },
-  { "akinsho/toggleterm.nvim", version = "*",                                      config = function() require(
-    "toggleterm").setup({ direction = "float" }) end },
+  { "stevearc/overseer.nvim", config = function() require("overseer").setup() end },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require(
+        "toggleterm").setup({ direction = "float" })
+    end
+  },
 
   { "mfussenegger/nvim-dap" },
   {
@@ -163,36 +193,106 @@ require("lazy").setup({
     end
   },
 
-  { "folke/neodev.nvim",            config = function() require("neodev").setup() end },
+  { "folke/neodev.nvim",      config = function() require("neodev").setup() end },
 
-  { "goolord/alpha-nvim",           config = function() require("alpha").setup(require("alpha.themes.startify").config) end },
+  { "goolord/alpha-nvim",     config = function() require("alpha").setup(require("alpha.themes.startify").config) end },
 
-  { "folke/persistence.nvim",       config = function() require("persistence").setup() end },
+  { "folke/persistence.nvim", config = function() require("persistence").setup() end },
 
-  { "theprimeagen/harpoon",         branch = "harpoon2",                                                                    dependencies = { "nvim-lua/plenary.nvim" },             config = function()
-    require("harpoon"):setup({}) end },
-
-  { "tomasky/bookmarks.nvim",       dependencies = { "nvim-telescope/telescope.nvim" },                                     config = function()
-    require("bookmarks").setup({}) end },
-
-  { "anuvyklack/fold-preview.nvim", dependencies = { "anuvyklack/keymap-amend.nvim" },                                      config = function()
-    require("fold-preview").setup() end },
-{ "karb94/neoscroll.nvim", config = function()
-    local neoscroll = require("neoscroll")
-    neoscroll.setup({
-      hide_cursor = true,
-      performance_mode = true,
-    })
-
-    local mappings = {
-      ["<C-y>"] = function() neoscroll.scroll(-1, { move_cursor = false, duration = 50 }) end,
-      ["<C-e>"] = function() neoscroll.scroll( 1, { move_cursor = false, duration = 50 }) end,
-    }
-
-    for key, fn in pairs(mappings) do
-      vim.keymap.set({ "n", "v", "x" }, key, fn, { silent = true })
+  {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup({})
     end
-  end },
+  },
+
+  {
+    "tomasky/bookmarks.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("bookmarks").setup({})
+    end
+  },
+
+  {
+    "anuvyklack/fold-preview.nvim",
+    dependencies = { "anuvyklack/keymap-amend.nvim" },
+    config = function()
+      require("fold-preview").setup()
+    end
+  },
+  -- DAP virtual text
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = { "markdown" }
+  },
+
+  -- UFO folding
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      require("ufo").setup()
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end
+  },
+
+  -- neotest
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/nvim-nio",
+      "nvim-neotest/neotest-python",
+      "Issafalcon/neotest-dotnet"
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({}),
+          require("neotest-dotnet")({})
+        }
+      })
+    end
+  },
+
+  -- ThePrimeagen's vim-be-good
+  { "ThePrimeagen/vim-be-good" },
+
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      local neoscroll = require("neoscroll")
+      neoscroll.setup({
+        hide_cursor = true,
+        performance_mode = true,
+      })
+
+      local mappings = {
+        ["<C-y>"] = function() neoscroll.scroll(-1, { move_cursor = false, duration = 50 }) end,
+        ["<C-e>"] = function() neoscroll.scroll(1, { move_cursor = false, duration = 50 }) end,
+      }
+
+      for key, fn in pairs(mappings) do
+        vim.keymap.set({ "n", "v", "x" }, key, fn, { silent = true })
+      end
+    end
+  },
 })
 
 local cmp_caps = require("cmp_nvim_lsp").default_capabilities()
@@ -239,7 +339,7 @@ vim.keymap.set("n", "<leader>fg", function() require("telescope.builtin").live_g
 vim.keymap.set("n", "<leader>fb", function() require("telescope.builtin").buffers() end, { silent = true })
 vim.keymap.set("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, { silent = true })
 vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { silent = true })
-vim.keymap.set("n", "<leader>fd", function() require("telescope.builtin").diagnostics() end, { silent = true })
+-- redundant vim.keymap.set("n", "<leader>fd", function() require("telescope.builtin").diagnostics() end, { silent = true })
 
 vim.keymap.set({ "n", "v" }, "<leader>ca", function() require("actions-preview").code_actions() end, { silent = true })
 
@@ -295,62 +395,78 @@ local wk = require("which-key")
 
 wk.add({
   -- File explorer
-  { "<leader>e",  function() require("oil").open() end,                     desc = "File Explorer" },
+  { "<leader>e",  function() require("oil").open() end,                          desc = "File Explorer" },
 
   -- Find
   { "<leader>f",  group = "Find" },
-  { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find Files" },
-  { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Grep Text" },
-  { "<leader>fb", function() require("telescope.builtin").buffers() end,    desc = "Find Buffers" },
-  { "<leader>fh", function() require("telescope.builtin").help_tags() end,  desc = "Help Tags" },
-  { "<leader>fd", "<cmd>Telescope diagnostics<CR>",                         desc = "Diagnostics" },
-  { "<leader>ft", "<cmd>TodoTelescope<CR>",                                 desc = "TODOs" },
+  { "<leader>ff", function() require("telescope.builtin").find_files() end,      desc = "Find Files" },
+  { "<leader>fg", function() require("telescope.builtin").live_grep() end,       desc = "Grep Text" },
+  { "<leader>fb", function() require("telescope.builtin").buffers() end,         desc = "Find Buffers" },
+  { "<leader>fh", function() require("telescope.builtin").help_tags() end,       desc = "Help Tags" },
+  { "<leader>fd", "<cmd>Telescope diagnostics<CR>",                              desc = "Diagnostics" },
+  { "<leader>ft", "<cmd>TodoTelescope<CR>",                                      desc = "TODOs" },
 
   -- LSP
   { "<leader>s",  group = "LSP" },
-  { "<leader>sg", vim.lsp.buf.definition,                                   desc = "Go to Definition" },
-  { "<leader>si", vim.lsp.buf.implementation,                               desc = "Go to Implementation" },
-  { "<leader>sR", vim.lsp.buf.references,                                   desc = "References" },
-  { "<leader>sK", vim.lsp.buf.hover,                                        desc = "Hover Info" },
-  { "<leader>sr", vim.lsp.buf.rename,                                       desc = "Rename Symbol" },
-  { "<leader>sa", function() require("actions-preview").code_actions() end, desc = "Code Action" },
-  { "<leader>sd", "<cmd>Trouble diagnostics toggle<cr>",                    desc = "Diagnostics List" },
+  { "<leader>sg", vim.lsp.buf.definition,                                        desc = "Go to Definition" },
+  { "<leader>si", vim.lsp.buf.implementation,                                    desc = "Go to Implementation" },
+  { "<leader>sR", vim.lsp.buf.references,                                        desc = "References" },
+  { "<leader>sK", vim.lsp.buf.hover,                                             desc = "Hover Info" },
+  { "<leader>sr", vim.lsp.buf.rename,                                            desc = "Rename Symbol" },
+  { "<leader>sa", function() require("actions-preview").code_actions() end,      desc = "Code Action" },
+  { "<leader>sd", "<cmd>Trouble diagnostics toggle<cr>",                         desc = "Diagnostics List" },
 
   -- Git
   { "<leader>g",  group = "Git" },
-  { "<leader>gs", function() require("gitsigns").stage_hunk() end,          desc = "Stage Hunk" },
-  { "<leader>gu", function() require("gitsigns").undo_stage_hunk() end,     desc = "Undo Stage" },
-  { "<leader>gr", function() require("gitsigns").reset_hunk() end,          desc = "Reset Hunk" },
-  { "<leader>gp", function() require("gitsigns").preview_hunk_inline() end, desc = "Preview Hunk" },
-  { "<leader>gb", function() require("gitsigns").blame_line() end,          desc = "Blame Line" },
-  { "<leader>gd", function() require("gitsigns").diffthis() end,            desc = "Diff File" },
+  { "<leader>gs", function() require("gitsigns").stage_hunk() end,               desc = "Stage Hunk" },
+  { "<leader>gu", function() require("gitsigns").undo_stage_hunk() end,          desc = "Undo Stage" },
+  { "<leader>gr", function() require("gitsigns").reset_hunk() end,               desc = "Reset Hunk" },
+  { "<leader>gp", function() require("gitsigns").preview_hunk_inline() end,      desc = "Preview Hunk" },
+  { "<leader>gb", function() require("gitsigns").blame_line() end,               desc = "Blame Line" },
+  { "<leader>gd", function() require("gitsigns").diffthis() end,                 desc = "Diff File" },
 
   -- Window
   { "<leader>w",  group = "Window" },
-  { "<leader>ww", "<C-w>w",                                                 desc = "Next Window" },
-  { "<leader>wh", "<C-w>h",                                                 desc = "Left Window" },
-  { "<leader>wj", "<C-w>j",                                                 desc = "Down Window" },
-  { "<leader>wk", "<C-w>k",                                                 desc = "Up Window" },
-  { "<leader>wl", "<C-w>l",                                                 desc = "Right Window" },
-  { "<leader>ws", "<cmd>split<CR>",                                         desc = "Horizontal Split" },
-  { "<leader>wv", "<cmd>vsplit<CR>",                                        desc = "Vertical Split" },
-  { "<leader>wq", "<C-w>q",                                                 desc = "Close Window" },
-  { "<leader>wo", "<C-w>o",                                                 desc = "Only Window" },
-  { "<leader>w=", "<C-w>=",                                                 desc = "Balance Windows" },
+  { "<leader>ww", "<C-w>w",                                                      desc = "Next Window" },
+  { "<leader>wh", "<C-w>h",                                                      desc = "Left Window" },
+  { "<leader>wj", "<C-w>j",                                                      desc = "Down Window" },
+  { "<leader>wk", "<C-w>k",                                                      desc = "Up Window" },
+  { "<leader>wl", "<C-w>l",                                                      desc = "Right Window" },
+  { "<leader>ws", "<cmd>split<CR>",                                              desc = "Horizontal Split" },
+  { "<leader>wv", "<cmd>vsplit<CR>",                                             desc = "Vertical Split" },
+  { "<leader>wq", "<C-w>q",                                                      desc = "Close Window" },
+  { "<leader>wo", "<C-w>o",                                                      desc = "Only Window" },
+  { "<leader>w=", "<C-w>=",                                                      desc = "Balance Windows" },
 
   -- Tasks & Terminals
-  { "<leader>t",  group = "Tasks/Term" },
-  { "<leader>tt", "<cmd>OverseerToggle<CR>",                                desc = "Tasks Panel" },
-  { "<leader>tr", "<cmd>OverseerRun<CR>",                                   desc = "Run Task" },
-  { "<leader>tv", "<cmd>ToggleTerm<CR>",                                    desc = "Terminal" },
+  { "<leader>x",  group = "Tasks/Term" },
+  { "<leader>xt", "<cmd>OverseerToggle<CR>",                                     desc = "Tasks Panel" },
+  { "<leader>xr", "<cmd>OverseerRun<CR>",                                        desc = "Run Task" },
+  { "<leader>xv", "<cmd>ToggleTerm<CR>",                                         desc = "Terminal" },
+
+  -- Tests
+  { "<leader>T",  group = "Tests" },
+  { "<leader>tn", function() require("neotest").run.run() end,                   desc = "Run nearest test" },
+  { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run file tests" },
+  { "<leader>to", function() require("neotest").output.open() end,               desc = "Test Output" },
+  { "<leader>ts", function() require("neotest").summary.toggle() end,            desc = "Test Summary" },
+
+  -- Markdown
+  { "<leader>m",  group = "Markdown" },
+  { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>",                              desc = "MD Preview" },
+
+  -- Folding
+  { "zR",         function() require("ufo").openAllFolds() end,                  desc = "Open all folds" },
+  { "zM",         function() require("ufo").closeAllFolds() end,                 desc = "Close all folds" },
 
   -- Debug
+
   { "<leader>d",  group = "Debug" },
-  { "<leader>db", function() require("dap").toggle_breakpoint() end,        desc = "Toggle Breakpoint" },
-  { "<leader>dc", function() require("dap").continue() end,                 desc = "Continue Debug" },
-  { "<leader>do", function() require("dap").step_over() end,                desc = "Step Over" },
-  { "<leader>di", function() require("dap").step_into() end,                desc = "Step Into" },
-  { "<leader>dO", function() require("dap").step_out() end,                 desc = "Step Out" },
+  { "<leader>db", function() require("dap").toggle_breakpoint() end,             desc = "Toggle Breakpoint" },
+  { "<leader>dc", function() require("dap").continue() end,                      desc = "Continue Debug" },
+  { "<leader>do", function() require("dap").step_over() end,                     desc = "Step Over" },
+  { "<leader>di", function() require("dap").step_into() end,                     desc = "Step Into" },
+  { "<leader>dO", function() require("dap").step_out() end,                      desc = "Step Out" },
 
   -- Utilities
   { "<leader>z",  group = "Utils" },
@@ -373,6 +489,6 @@ wk.add({
   },
 })
 
-vim.keymap.set("n", "<leader>f", function()
+vim.keymap.set("n", "<leader>fa", function()
   vim.lsp.buf.format({ async = true })
 end, { silent = true })
